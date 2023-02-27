98d62ff71e15721d994ee9e15fa72cfd9858b4ac
Tyler Jefferson <tylerrjefferson00@gmail.com>
2023-02-27 12:42:51 -0500

    banner image and title displayed w animated text

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,10 +1,48 @@
 import React from 'react';
+// images
+import Image from '../assets/avatar.svg';
+// icons
+import { FaGithub, FaLinkedin, FaInstagram } from 'react-icons/fa';
+// animation
+import { TypeAnimation } from 'react-type-animation';
+// variants
+import { fadeIn } from '../variants';
 
 const Banner = () => {
   return (
-    <div className='section' id='home'>
-      Banner
-    </div>
+    <section className='section' id='home'>
+      <div className='container mx-auto'>
+        <div>
+          {/* text */}
+          <div>
+            <h1>
+              TYLER <span>JEFFERSON</span>
+            </h1>
+            <div className='mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1]'>
+              <span className='text-white mr-3'>I am a</span>
+              <TypeAnimation 
+                sequence={[
+                  'Passionate developer',
+                  2000,
+                  'Highly motivated worker',
+                  2000,
+                  'Team player',
+                  2000
+                ]}
+                speed={50}
+                className='text-accent'
+                wrapper='span'
+                repeat={Infinity}
+              />
+            </div>
+          </div>
+          {/* image */}
+          <div>
+            <img src={Image} alt="profile image" />
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
