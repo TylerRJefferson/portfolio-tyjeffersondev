e2bf26d436d0017397488567386c491be5f5ccc0
Tyler Jefferson <tylerrjefferson00@gmail.com>
2023-03-03 11:37:12 -0500

    structure setup for about section

--- src/components/About.js
+++ src/components/About.js
@@ -9,9 +9,19 @@ import { motion } from 'framer-motion';
 import { fadeIn } from '../variants';
 
 const About = () => {
+  const [ref, inView] = useInView({
+    threshold: 0.5,
+  });
   return (
-    <section className='section' id='about'>
-      About
+    <section className='section' id='about' ref={ref}>
+      <div className='container mx-auto'>
+        <div>
+          {/* img */}
+          <div>img here</div>
+          {/* text */}
+          <div>text here</div>
+        </div>
+      </div>
     </section>
   );
 };
